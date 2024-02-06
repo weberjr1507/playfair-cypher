05218776d1d855b1f3d586b5b672c593c37a169c
weberjr1507 <weberjr1507@gmail.com>
2024-02-05 21:08:28 -0600

    updated progress: finished input sanitization, playfair matrix WIP

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 string promptInput() {
@@ -31,15 +32,68 @@ string promptInput() {
 //Remove all spaces
 //Separate string into pairs of two (possibly put each pair in an array)
 //Ask for key
-//Construct table (how to represent in data for appropriate purposes? 2D array?)
-//
+//Construct table (how to represent in data for appropriate purposes? 2D array?)//
 
+string promptKeyWord() {
+    string keyWord;
+    cout << "Enter the keyword of the cipher: ";
+    getline(std::cin,keyWord);
+    return keyWord;
+}
+
+string removeSpaces(string rawCipher) {
+    // string removedChars = remove(rawCipher.begin(), rawCipher.end(), ' '), rawCipher.end().c_str();
+    string cleansedCipher;
+    int pos = 0;
+    while (pos < rawCipher.size()) {
+        if (rawCipher[pos] == ' ') {
+            rawCipher[pos] = rawCipher[pos+1];
+        } else {
+            cleansedCipher += rawCipher[pos];
+        }
+        pos += 1;
+    }
+    return cleansedCipher;
+}
+
+vector<string> divideIntoTwos(string rawCipher) {
+   vector<string> cipherPairs;
+   int pos = 0;
+   while (pos < rawCipher.length()) {
+    cipherPairs.push_back(rawCipher.substr(pos, 2));
+    pos+=2;
+   }
+   string cipherString = " ";
+   for (const string& pair : cipherPairs) {
+    cipherString += pair + " ";
+   }
+   return cipherPairs;
+}
+
+char generateMatrix(string cipherKey) {
+    const int row = 5;
+    const int col = 5;
+    
+    char matrix[row][col];
+    
+}
+
+string decipherWithPlayFair(string cipher) {
+    string cipherKey = promptKeyWord();
+    cipher = removeSpaces(cipher);
+    vector<string> cipherPairs = divideIntoTwos(cipher);
+    
+
+
+    return "";
+}
 
 //bonus: save original spacing to reapply at the end
 
 int main() {
     string cipher = promptInput();
-    cout << cipher << endl; 
+    string decryptedCipher = decipherWithPlayFair(cipher);
+    // cout << cipher << endl;
     return 0;
 }
 
